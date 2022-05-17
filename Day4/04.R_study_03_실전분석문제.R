237ac5174c6e2e541017c575f57f4aaf9e60200a
HanBin1205 <diary1205@naver.com>
2022-05-17 17:49:19 +0900

    220516 월요일 R 실전분석문제

--- Day4/04.R_study_03_실전분석문제.R
+++ Day4/04.R_study_03_실전분석문제.R
@@ -19,3 +19,46 @@ avg.smoker
 idx <- which(tips$smoker == 'No')
 avg.nonsmoker <- mean(tips[idx, 'tip'])
 avg.nonsmoker
+
+meanbycol.tip <- function(tips, colname){ }
+
+meanbycol.tip <- function(colname) {
+  value <- unique(tips[, colname]) # ① 값의 종류를 구함
+  result <- list()
+  for(i in 1:length(value)) { # ② 값의 종류별로 평균을 구함
+    idx <- which(tips[,colname] == value[i]) # ②-1
+    result[i] <- mean(tips[idx,'tip']) # ②-2
+  }
+  names(result) <- value # ③ 결과값에 이름을 붙임
+  return(result)
+}
+
+source('myfunc.R')
+meanbycol.tip('sex')
+
+meanbycol.tip('smoker')
+meanbycol.tip('size')
+meanbycol.tip('day')
+  
+categorize.tip <- function(tips){
+  tip_ratio <- tips$tip/tips$total_bill * 100
+  class <- c()
+  for(i in 1:nrow(tips)){
+    if(tip_ratio[i] < 10){
+      class[i] <- 1
+    }else if(tip_ratio[i] < 15){
+      class[i] <- 2
+    }else if(tip_ratio[i] < 20){
+      class[i] <- 3
+    }else{
+      class[i] <- 4
+    }
+  }
+  tips.new <- cbind(tips, type = class, ratio = tip_ratio)
+  return(tips.new)
+}
+  
+source('myfunc.R')
+tips.new <- categorize.tip(tips)
+head(tips.new)
+  
